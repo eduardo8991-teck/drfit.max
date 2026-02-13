11aa17b9fa93064e8bdf22cb16bbc943fff4708c
eduardo8991-teck <eduardo.8991@colegioprogresso.g12.br>
2026-02-13 00:37:23 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,26 @@
+function guardar() {
+    const nome = document.getElementById("nome").value;
+    const senha = document.getElementById("senha").value;
+
+    console.log("Nome:", nome);
+    console.log("Senha:", senha);
+}
+
+
+
+
+
+/*function guardar() {
+    console.log(document.getElementById("text").value);
+}
+/*curiosidade o ola mundo aqui em baixo o vscd criou sozinho */
+/*E ele colocou até isso <button onclick="mostrar()">Mostrar</button>*/
+/*function mostrar() {
+    document.getElementById("text").value = "seu nome aqui";
+}
+/*    <input id="text">
+<button onclick="guardar()">Salvar</button>
+<button onclick="mostrar()">Mostrar</button>
+
+<script src="codigo.js"></script>
+  */  
